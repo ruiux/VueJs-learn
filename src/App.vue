c74f297af70b82cb26cb787afe7e8b61b8079b74
unknown <ridomulanda@gmail.com>
2020-05-05 22:03:38 +0700

    basic learn

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row header mb-3 p-3">
+        <div class="col-lg-12">
+          <h2>Detail Tugas</h2>
+        </div>
+      </div>
+      <div class="row content">
+        <div class="col-lg-12">
+          <div class="list-tugas text-left p-2">
+            <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas pb-3">
+
+              <itemTugas v-bind:tugasSaya="tugasSaya"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import itemTugas from './components/itemTugas.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function() {
+    return {
+      tugas: [
+        {
+          id: 1,
+          namaTugas: "Ngoding",
+          isDone: false
+        },
+        {
+          id: 2,
+          namaTugas: "Kerjain Live",
+          isDone: false
+        },
+        {
+          id: 3,
+          namaTugas: "Makan",
+          isDone: false
+        },
+        {
+          id: 4,
+          namaTugas: "Tidur",
+          isDone: false
+        }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    itemTugas
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +66,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header {
+  background-color: #1abc9c;
+}
+.header h2 {
+  margin: 0;
+}
+.item-completed {
+  color: #1abc9c;
 }
 </style>
---
+++ src/components/itemTugas.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-check">
+    <input class="form-check-input" onclick type="checkbox" value id="defaultCheck1" />
+    <label
+      class="form-check-label"
+      v-bind:class="{'item-completed':tugasSaya.isDone}"
+      for="defaultCheck1"
+    >{{tugasSaya.namaTugas}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "itemTugas",
+    props:[
+        "tugasSaya"
+    ]
+};
+</script>
+
+<style scoped>
+</style>
